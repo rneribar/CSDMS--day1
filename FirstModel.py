294e0ed4f60a7d8d9e9ee33f89d6744f272db6c8
Raquel Neri Barranco <rneribar@asu.edu>
2025-05-07 23:31:14 +0000

    Add plot_profile function

--- FirstModel.py
+++ FirstModel.py
@@ -3,6 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def plot_profile(x,cake, color='r'):
+    '''Display the cake profile'''
+    plt.figure()
+    plt.plot(x,cake,color)
+    plt.xlabel("x")
+    plt.ylabel("cake")
+    plt.title("Cake profile")
+
+
 D=100
 Lx=300
 
@@ -16,11 +25,7 @@ C_right=0
 C[x<=Lx/2]=C_left
 C[x>Lx/2]=C_right
 
-plt.figure()
-plt.plot(x,C,"r")
-plt.xlabel("x")
-plt.ylabel("C")
-plt.title("Initial profile")
+plot_profile(x,C)
 
 nt=5000
 dt=0.5*dx**2/D
@@ -30,9 +35,4 @@ dt
 for t in range(0, nt):
 	C[1:-1] += D * dt / dx ** 2 * (C[:-2] - 2*C[1:-1] + C[2:])
 
-plt.figure()
-plt.plot(x,C,"r")
-plt.xlabel("x")
-plt.ylabel("C")
-plt.title("Initial profile")
-
+plot_profile(x,C, color='blue')
