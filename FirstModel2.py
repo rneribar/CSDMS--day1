66ba96b311e4027be979c36bd6eaeaa274005687
Raquel Neri Barranco <rneribar@asu.edu>
2025-05-08 17:32:45 +0000

    Factored script into testable functions

--- FirstModel2.py
+++ FirstModel2.py
@@ -2,6 +2,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def calculate_stable_time_step(dx, diffusivity):
+    return 0.5*dx**2/diffusivity
+
+def step_like(x, step_at=0):
+    y=np.empty_like(x, dtype=float)
+
+    y[:step_at]=0.0
+    y[step_at]=0.5
+    y[step_at + 1 :]=1.0
+
+    return y
+
+def new_profile(x,form='step'):
+    match form:
+        case 'step':
+            return step_like(x,step_at=len(x)//2)
+        case _:
+            raise ValueError(f'unknown profile type{form}')
 
 def plot_profile(x,cake, color='r'):
     '''Display the cake profile'''
@@ -19,18 +37,13 @@ dx=0.5
 x=np.arange(start=0, stop=Lx, step=dx)
 nx=len(x)
 
-C=np.zeros_like(x)
-C_left=500
-C_right=0
-C[x<=Lx/2]=C_left
-C[x>Lx/2]=C_right
+C=new_profile(x, form='step')
+
 
 plot_profile(x,C)
 
 nt=5000
-dt=0.5*dx**2/D
-
-dt
+dt = calculate_stable_time_step(dx,D)
 
 for t in range(0, nt):
 	C[1:-1] += D * dt / dx ** 2 * (C[:-2] - 2*C[1:-1] + C[2:])
